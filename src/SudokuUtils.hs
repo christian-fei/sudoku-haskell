bde9cc561db48c3cf8a1067fc2906560306a210c
Christian Fei <crifei93@gmail.com>
2015-05-14 19:56:16 +0200

    refactored with function application $

--- src/SudokuUtils.hs
+++ src/SudokuUtils.hs
@@ -5,4 +5,4 @@ existsInRow :: [[Int]] -> Int -> Int -> Bool
 existsInRow cs r n = elem n (cs !! r)
 
 existsInColumn :: [[Int]] -> Int -> Int -> Bool
-existsInColumn ss c n = elem n (map (\row -> row !! c) ss)
+existsInColumn ss c n = elem n $ map (\row -> row !! c) ss
