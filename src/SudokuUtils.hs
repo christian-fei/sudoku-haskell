c91c80739c84f560ead666730725a2d91f806ccb
Christian Fei <crifei93@gmail.com>
2015-05-14 20:00:26 +0200

    refactored existsInColumn to flatten function

--- src/SudokuUtils.hs
+++ src/SudokuUtils.hs
@@ -5,4 +5,5 @@ existsInRow :: [[Int]] -> Int -> Int -> Bool
 existsInRow cs r n = elem n (cs !! r)
 
 existsInColumn :: [[Int]] -> Int -> Int -> Bool
-existsInColumn ss c n = elem n $ map (\row -> row !! c) ss
+existsInColumn ss c n = elem n $ flatten ss c
+  where flatten ss c = map (\row -> row !! c) ss
