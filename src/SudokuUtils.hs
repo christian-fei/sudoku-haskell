1b9896afa05ec866beda20e85fcb585107df4722
Christian Fei <crifei93@gmail.com>
2015-05-15 14:21:41 +0200

    renamed pickColumn

--- src/SudokuUtils.hs
+++ src/SudokuUtils.hs
@@ -5,5 +5,5 @@ existsInRow :: [[Int]] -> Int -> Int -> Bool
 existsInRow matrix row n = elem n (matrix !! row)
 
 existsInColumn :: [[Int]] -> Int -> Int -> Bool
-existsInColumn matrix column n = elem n $ flatten matrix column
-  where flatten matrix column = map (\row -> row !! column) matrix
+existsInColumn matrix column n = elem n $ pickColumn matrix column
+  where pickColumn matrix column = map (\row -> row !! column) matrix
